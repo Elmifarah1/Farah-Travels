01d032985035d55d8f451f78ef6afe9e02978e82
Elmi Farah <elmifarah98@gmail.com>
2024-09-17 00:05:12 +0000

    changed debug to false

--- farah_travels/settings.py
+++ farah_travels/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-elmifarah1-farahtravels-r246anpbl28.ws-eu116.gitpod.io',
